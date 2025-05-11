653f9bac094fca66e5acc1c386166bd6357ef48c
syedubaid <syedabuubaid@gmail.com>
2025-05-11 14:02:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const express=require('express')
 const httpProxy=require('http-proxy')
+const cookieParser=require('cookie-parser');
+
 
 const app=express();
+app.use(cookieParser());
 const PORT=8000;
 
 const BASE_PATH="https://runix-v2.s3.ap-south-1.amazonaws.com/__outputs"
 
 const proxy=httpProxy.createProxy()
 
-let projectId=null;
-
 app.listen(PORT || process.env.reversePorxyPORT ,()=>{
     console.log('Reverse Proxy listening on PORT:',PORT)
 })
 
-//middleware to catch the project id
-app.use('/p/:projectId',(req,res,next)=>{
-    req.projectId=req.params.projectId;
-    next();
-})
-
 // proxy for the intial index.html request
 app.use('/p/:projectId',(req,res)=>{
-    const resolvesTo=`${BASE_PATH}/${req.projectId}`
+    const { projectId }=req.params;
+    res.cookie('projectId',projectId);
+    const resolvesTo=`${BASE_PATH}/${projectId}`
+    console.log(resolvesTo);
     proxy.web(req,res,{target:resolvesTo, changeOrigin:true})
 })
 
 // proxy for assets folder
 app.use('/assets/',(req,res)=>{
-    const resolvesTo=`${BASE_PATH}/${req.projectId}/assets`
+    const projectId=req.cookies.projectId;
+    if(!projectId){
+        return res.status(400).send('Missing Project Id');
+    }
+    const resolvesTo=`${BASE_PATH}/${projectId}/assets`
+    console.log(resolvesTo);
     proxy.web(req,res,{target:resolvesTo, changeOrigin:true})
+    
 })
 
 
